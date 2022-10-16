a7402962f72455ca6f8c9cce5f437a24af0be7e5
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2022-10-16 23:55:53 +0530

    ui: init mantine (#2)

--- apps/test/pages/_app.tsx
+++ apps/test/pages/_app.tsx
@@ -1,20 +1,47 @@
-// import App from 'next/app'
 import '../styles/globals.css';
+import { MantineProvider } from 'ui';
+
+// I'm disabling preflight in tailwindcss rather than using emotionCache
+
+/*
+import { MantineProvider, createEmotionCache } from '@mantine/core';
+
+const myCache = createEmotionCache({
+  key: 'mantine',
+  prepend: false,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider emotionCache={myCache}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
+*/
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <MantineProvider withNormalizeCSS withGlobalStyles>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+};
+
+/*
+Only uncomment this method if you have blocking data requirements for
+every single page in your application. This disables the ability to
+perform automatic static optimization, causing every page in your app to
+be server-side rendered.
+
+import App from 'next/app'
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps }
 }
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
+*/
 
 export default MyApp;
