8fa29ec11b43d07148d97a587df2029358f07de6
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-10-16 18:47:53 +0530

    test: making test repo cleaner

--- apps/test/pages/index.tsx
+++ apps/test/pages/index.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link';
-import Layout from '../components/Layout';
+import React from 'react';
+import { Button } from 'ui';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href='/about'>
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+const Test = () => {
+  return (
+    <div className='bg-red-500'>
+      hello
+      <Button />
+    </div>
+  );
+};
 
-export default IndexPage;
+export default Test;
