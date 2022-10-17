46d34b5bf1c410a6010a3df5766b8f82d886ec1f
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2022-10-17 22:16:41 +0530

    init: mantine in UI package (#3)

--- apps/test/pages/index.tsx
+++ apps/test/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button } from 'ui';
+import { Button, TextEditor } from 'ui';
 
 const Test = () => {
   return (
-    <div className='bg-gray-100 min-h-screen'>
+    <div className='bg-gray-100 min-h-screen container m-auto'>
+      <TextEditor />
       <Button />
     </div>
   );
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button';
+export { default as TextEditor } from './components/TextEditor';
 export { MantineProvider } from '@mantine/core';
