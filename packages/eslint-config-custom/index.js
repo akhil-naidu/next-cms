1eea628851b5e944f7c5c9c35b8d4d74770fa92c
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-10-19 10:22:37 +0530

    chore: prettier and eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['next', 'turbo', 'prettier', 'next/babel', 'next/core-web-vitals'],
   rules: {
+    'no-console': 2,
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
   },
